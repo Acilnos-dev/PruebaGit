8e8e4f228da205b5871f1a2ca667945c568ec81e
Acilnos-dev <nmestre2007@gmail.com>
2025-10-27 13:15:53 +0100

    prueba2

--- prubecita.py
+++ prubecita.py
@@ -1,2 +1,9 @@
-def frase(frase:str) -> int:
-    for i in frase
+def frase1(frase:str) -> float:
+    precio = 0
+    for i in frase:
+        if i == "a""e""i""o""u":
+            precio += 1
+        elif i == frase.lower:
+            precio += 0.5
+    return precio
+print (frase1("Hola Mundo"))
